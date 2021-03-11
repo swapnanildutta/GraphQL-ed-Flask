ee54ed530f717f2f75109e0a8953036fd1b19404
Swapnanil Dutta <swapnanildutta2000@gmail.com>
2021-03-12 00:03:40 +0530

    Setups

---
+++ books/models/books.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import  backref, relationship
+
+from ..database.base import Base
+from ..models.genres import Genres
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    author = Column(String)
+    genre_id = Column(Integer, ForeignKey('genres.id'))
+    genre = relationship(
+        Genres,
+        backref=backref('books', uselist=True, cascade='delete,all')
+    )
